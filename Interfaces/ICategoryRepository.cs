95a6b65e0e72821db8082fe5e0c218526b48b0b2
codyh587 <108317527+codyh587@users.noreply.github.com>
2023-06-12 12:19:06 -0700

    Added PUT method for Category

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -9,6 +9,7 @@ namespace OnlineStoreAPI.Interfaces
         ICollection<Product> GetProductByCategory(int categoryId);
         bool CategoryExists(int id);
         bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
         bool Save();
     }
 }
